aa31d92a70f91d3a299f7dc42f789e8b85c5be69
trungnguyen117 <22024553@vnu.edu.vn>
2026-04-09 10:31:04 +0700

    fix astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://yourdomain.com',
-  output: 'hybrid',
+  site: "https://my-blog-vert-sigma.vercel.app",
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [sitemap()],
   image: {
-    remotePatterns: [
-      { protocol: 'https', hostname: '**.amazonaws.com' },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "**.amazonaws.com" }],
   },
 });
